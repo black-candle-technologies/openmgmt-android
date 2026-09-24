e53308af3921f454be1c661691b1fe546cb8f679
Muse <muse@blackcandletech.com>
2026-09-23 23:14:38 -0700

    Bump to 0.3.3 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.openmgmt.android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.3.2"
+        versionCode = 7
+        versionName = "0.3.3"
     }
 
     buildTypes {
